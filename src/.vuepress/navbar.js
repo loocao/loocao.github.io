8dbf498ac43ddd98a2e1fae63af5391b418c940d
loocao <loocao@yeah.net>
2024-01-19 17:35:04 +0800

    更新版本

--- src/.vuepress/navbar.js
+++ src/.vuepress/navbar.js
@@ -3,6 +3,6 @@ import { navbar } from 'vuepress-theme-hope'
 export default navbar([
   '/',
   { text: '🫕 Develop', link: '/Develop/' },
-  { text: '🌝 绳命在于折腾', link: '/绳命在于折腾/' },
+  { text: '🌝 绳命在于折腾', link: '/life/' },
   { text: '📘 随手记', link: '/note/' }
 ])
--- src/.vuepress/sidebar.js
+++ src/.vuepress/sidebar.js
@@ -1,16 +1,10 @@
 import { sidebar } from 'vuepress-theme-hope'
 
 export default sidebar({
-  '/': [
-    { text: 'Docker', icon: 'laptop-code', prefix: 'docker/', link: 'docker/', children: 'structure' },
-    { text: '2023', icon: 'book', prefix: '2023/', children: 'structure' },
-    'docker'
-  ],
   '/Develop/': [
-    '',
     { text: '🐧 Linux', prefix: 'Linux/', children: 'structure' },
     { text: '🍬 其它', prefix: 'others/', children: 'structure' }
   ],
-  '/绳命在于折腾/': ['', { text: 'NAS', prefix: '绳命在于折腾/', children: 'structure' }],
+  '/life/': [{ text: 'NAS', prefix: 'NAS/', children: 'structure' }],
   '/note/': 'structure'
 })
