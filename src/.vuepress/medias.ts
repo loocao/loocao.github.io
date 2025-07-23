f9cbe4f06b1ed56219497e273da258590236e93c
loocao <dev@loocao.cn.eu.org>
2025-07-23 21:36:36 +0800

    添加Rentry

--- src/.vuepress/medias.ts
+++ src/.vuepress/medias.ts
@@ -1,6 +1,14 @@
 export default {
   GitHub: 'https://github.com/loocao',
   Github: 'https://gist.github.com/loocao',
+  Text: {
+    icon: 'https://text.is/static/icon/256.png',
+    link: 'https://text.is/LV14O',
+  },
+  Rentry: {
+    icon: 'https://rentry.org/favicon.ico',
+    link: 'https://rentry.org/6p7zuy5v',
+  },
   Listly: {
     icon: 'https://d28efpdu2tk2gz.cloudfront.net/assets/logos/listly-square-logo-afe2de8d2e2ecadd3816e16e2254f6bd8b6fb39ffd190be5bdd15174d109f614.png',
     link: 'https://list.ly/loocao',
@@ -8,10 +16,6 @@ export default {
   LinkTree: {
     icon: 'https://assets.production.linktr.ee/9a495cba4f6102bd9557614cb028737416b4a9d4/favicon/favicon.png',
     link: 'https://linktr.ee/loocao',
-  },
-  'Text.is': {
-    icon: 'https://text.is/static/icon/256.png',
-    link: 'https://text.is/LV14O',
   },
   EgoLinks: {
     icon: 'https://egolinks.online/assets/linkstack/images/favicon_1749129352.ico',
@@ -21,7 +25,7 @@ export default {
     icon: 'https://linkstack.lgbt/assets/linkstack/images/avatar_1705433782.png',
     link: 'https://linkstack.lgbt/@loocao',
   },
-  'LinkStack': {
+  LinkStack: {
     icon: 'https://linksta.cc/assets/linkstack/images/logo.svg',
     link: 'https://linksta.cc/@loocao',
   },
